2ba414352ad20905653f300d96f4b526dc251148
ChaseSobey <Christopher.Chase.Sobey@gmail.com>
2022-11-06 17:31:22 +0000

    rock_paper_scissors rubocop formatting changes

--- RB101/Lesson-2/rock_paper_scissors.rb
+++ RB101/Lesson-2/rock_paper_scissors.rb
@@ -1,57 +1,52 @@
-# ROCK PAPER SCISSORS GAME
+#ROCK PAPER SCISSORS GAME
 
 # Ask user to play rock paper or scissors
 # Computer makes a move
 # Then winner is displayed
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
-
+VALID_CHOICES = %w(rock paper scissors)
 # Method Definitions
 
 def prompt(words)
   puts "=> #{words}"
-end 
+end
+
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
 
 def display_results(player_choice, computer_choice)
-  if player_choice == computer_choice
-    prompt('Its a tie!!!')
-  elsif (player_choice == 'rock' && computer_choice == 'scissors') || 
-    (player_choice == 'paper' && computer_choice == 'rock') || 
-    (player_choice == 'scissors' && computer_choice == 'paper')
+  if win?(player_choice, computer_choice)
     prompt('You won!')
+  elsif win?(computer_choice, player_choice)
+    prompt('Computer won!')
   else
-    prompt("Computer won!")
-  end 
-end 
-
-def prompt(words)
-  puts "=> #{words}"
-end 
-
+    prompt("It's a tie!!")
+  end
+end
 player_choice = ''
-loop do 
-  
-  loop do 
+loop do
+  loop do
     prompt("Please choose #{VALID_CHOICES.join(', ')}")
     player_choice = gets.chomp.downcase
-  
-    if VALID_CHOICES.include?(player_choice.downcase)
-      break
-    else
-      prompt("Thats not a valid choice")
-    end 
-  end 
-  
-  computer_choice = VALID_CHOICES.sample
-  
-  prompt("you chose: #{player_choice}, the computer chose: #{computer_choice}")
-  
-  display_results(player_choice, computer_choice)
-  
-  prompt("Play again? (y/n)")
-  play_again = gets.chomp
-  
-  break unless play_again.downcase == 'y'
-end 
 
-prompt("Thanks for playing!")
+    break if VALID_CHOICES.include?(player_choice)
+
+    prompt('Thats not a valid choice')
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("you chose: #{player_choice}, the computer chose: #{computer_choice}")
+
+  display_results(player_choice, computer_choice)
+
+  prompt('Play again? (y/n)')
+  play_again = gets.chomp
+
+  break unless play_again.downcase == 'y'
+end
+
+prompt('Thanks for playing!')
